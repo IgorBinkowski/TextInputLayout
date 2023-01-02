adb01e263dba0d96764d957c51e096b8dbc859ac
Dominos0321 <dsch792@gmail.com>
2023-01-02 14:39:05 +0100

    Hasła

--- app/src/main/java/com/ft_ss/textinputlayout/MainActivity.kt
+++ app/src/main/java/com/ft_ss/textinputlayout/MainActivity.kt
@@ -11,15 +11,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    private fun setupTextInputLayout() {
-        findViewById<Button>(R.id.Sprawdź).setOnClickListener {
-            val name = findViewById<TextInputEditText>(R.id.Imie).text.toString()
-            val Nick = findViewById<TextInputEditText>(R.id.Nick).text.toString()
-            val password = findViewById<TextInputEditText>(R.id.Hasło).text.toString()
-            val potwierdź = findViewById<TextInputEditText>(R.id.Potwierdź).text.toString()
 
-            findViewById<TextView>(R.id.textView).text = "Imie: $name, Nick: $Nick,Hasło: $password, Potwierdź: $potwierdź"
-            }
+        val name = findViewById<TextInputEditText>(R.id.Imie).text.toString()
+        val Nick = findViewById<TextInputEditText>(R.id.Nick).text.toString()
+        var password = findViewById<TextInputEditText>(R.id.Hasło).text.toString()
+        val potwierdź = findViewById<TextInputEditText>(R.id.Potwierdź).text.toString()
+
+            findViewById<Button>(R.id.Sprawdź).setOnClickListener {
+
+
+                if (password == potwierdź)
+                    findViewById<TextView>(R.id.textView).text = " Hasła: Zgodne"
+                else findViewById<TextView>(R.id.textView).text = " Hasła: Niezgodne"
         }
     }
+}
