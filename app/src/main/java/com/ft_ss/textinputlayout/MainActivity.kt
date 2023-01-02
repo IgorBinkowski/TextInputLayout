8ec63e8adb9b21c7c827901e5716705f70279880
Dominos0321 <dsch792@gmail.com>
2023-01-02 15:09:47 +0100

    Zgodność Hasła

--- app/src/main/java/com/ft_ss/textinputlayout/MainActivity.kt
+++ app/src/main/java/com/ft_ss/textinputlayout/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -14,15 +13,14 @@ class MainActivity : AppCompatActivity() {
 
         val name = findViewById<TextInputEditText>(R.id.Imie).text.toString()
         val Nick = findViewById<TextInputEditText>(R.id.Nick).text.toString()
-        var password = findViewById<TextInputEditText>(R.id.Hasło).text.toString()
-        val potwierdź = findViewById<TextInputEditText>(R.id.Potwierdź).text.toString()
 
             findViewById<Button>(R.id.Sprawdź).setOnClickListener {
-
-
-                if (password == potwierdź)
-                    findViewById<TextView>(R.id.textView).text = " Hasła: Zgodne"
-                else findViewById<TextView>(R.id.textView).text = " Hasła: Niezgodne"
+                var password = findViewById<TextInputEditText>(R.id.Hasło).text.toString()
+                var potwierdź = findViewById<TextInputEditText>(R.id.Potwierdź).text.toString()
+                if (password == potwierdź){
+                    findViewById<TextView>(R.id.textView).text = "Hasła: Zgodne"}
+                else {
+                    findViewById<TextView>(R.id.textView).text = " Hasła: Niezgodne"}
         }
     }
 }
